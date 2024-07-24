3b291615e4fdbd33d205500c997691f46f070cc8
Aadhar gaur <aadhar.g@rglabs.net>
2024-07-24 12:40:15 +0530

    Required external assets added & Login & Register Component added.

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,16 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css', 
+                'public/assets/css/adminlte.min.css', 
+                'public/assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css',
+                'public/assets/plugins/fontawesome-free/css/all.min.css',
+                'resources/js/app.js',
+                'public/assets/plugins/jquery/jquery.min.js',
+                'public/assets/js/adminlte.min.js',
+                'public/assets/plugins/bootstrap/js/bootstrap.bundle.min.js',
+            ],
             refresh: true,
         }),
         vue({ 
